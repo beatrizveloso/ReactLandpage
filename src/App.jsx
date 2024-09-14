25b387d710434cecda7f3b1969181ac9ff58250e
beatrizveloso <beatrizoslov6@gmail.com>
2024-09-14 16:34:33 -0300

    pokemonApi

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { useEffect } from 'react'
 import Example from './class/hooks/useEffect/example'
 import CountEffect from './class/hooks/useEffect/countEffect'
+import PokemonApi from './class/hooks/useHooks/pokemonApi'
 
 export default function App() {
 
     return (
-        <CountEffect />
+        <PokemonApi />
     )
 }
